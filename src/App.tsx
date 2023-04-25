ece21185ae16bddac128345c7ae9e179a35bfba4
stevecyj <ycsteven.tsao@gmail.com>
2023-04-25 17:20:20 +0800

    chore: set resolve alias

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import Navbar from './components/Navbar.tsx'
-import './App.css'
+import Navbar from '@/components/Navbar.tsx';
+import './App.css';
 
 function App() {
-  console.log(123)
+  console.log(123);
   return (
     <>
       <Navbar></Navbar>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
