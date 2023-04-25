7c23ae8797aae957fd15aedf16985ef5a68c7edb
stevecyj <ycsteven.tsao@gmail.com>
2023-04-25 20:27:46 +0800

    style: footer,body

--- src/layouts/Default.tsx
+++ src/layouts/Default.tsx
@@ -1,11 +1,18 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Flex } from '@chakra-ui/react';
 import Navbar from '@/components/Navbar.tsx';
+import Footer from '@/components/Footer.tsx';
 
 const DefaultLayout = () => {
   return (
-    <Box w='100%'>
-      <Navbar></Navbar>;
-    </Box>
+    <Flex w='100%' minH='100vh' direction={{ lg: 'column' }}>
+      <Box w={{ lg: '100%' }}>
+        <Navbar />
+      </Box>
+      <Box flexGrow={1}></Box>
+      <Box w={{ lg: '100%' }}>
+        <Footer />
+      </Box>
+    </Flex>
   );
 };
 
