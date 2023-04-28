a21e8100a4cefc8c6615c4aa82d83cdb0e8a7530
stevecyj <ycsteven.tsao@gmail.com>
2023-04-28 15:02:08 +0800

    refactor: refactor color definitions to a separate file in theme folder
    - Add a new file `theme/color.ts` with color definitions
    - Modify `theme/theme.ts` to use the new color definitions
    - Remove color definitions from `theme/theme.ts` and move them to `theme/color.ts`
    - Export the `colors` object from `theme/color.ts`
    - Import `colors` from `theme/color.ts` in `theme/theme.ts`
    
    Signed-off-by: stevecyj <ycsteven.tsao@gmail.com>

--- theme/theme.ts
+++ theme/theme.ts
@@ -1,12 +1,9 @@
 import { extendTheme } from '@chakra-ui/react';
+import colors from './color.ts';
 
-const theme = extendTheme({
-  colors: {
-    brand: {
-      100: '#f7fafc',
-      900: '#1a202c',
-    },
-  },
-});
+const customTheme = {
+  colors,
+};
+const theme = extendTheme(customTheme);
 
 export default theme;
