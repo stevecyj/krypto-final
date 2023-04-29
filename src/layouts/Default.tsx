36031fe2053e9855bf7d965f0c0a10ed7d2b41b1
stevecyj <ycsteven.tsao@gmail.com>
2023-04-29 21:02:16 +0800

    refactor: refactor layout using Flex and Box components
    - Add `Flex` import to `Default.tsx`
    - Change `Box` display to `flex` and `flexDirection` to `column`
    - Change `Box` flexGrow to `1`
    
    Signed-off-by: stevecyj <ycsteven.tsao@gmail.com>

--- src/layouts/Default.tsx
+++ src/layouts/Default.tsx
@@ -1,15 +1,15 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Flex } from '@chakra-ui/react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '@/components/Navbar.tsx';
 import Footer from '@/components/Footer.tsx';
 
 const DefaultLayout = () => {
   return (
-    <Box w='100%' minH='100vh'>
+    <Box w='100%' minH='100vh' display={'flex'} flexDirection={'column'}>
       <Box w={{ lg: '100%' }}>
         <Navbar />
       </Box>
-      <Box justifyContent='center'>
+      <Box justifyContent='center' flexGrow={1}>
         <Outlet />
       </Box>
       <Box w={{ lg: '100%' }}>
