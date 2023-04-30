d8dde9b30009aa94ce4f8d165b35bcc15dbe7125
stevecyj <ycsteven.tsao@gmail.com>
2023-04-30 09:05:50 +0800

    chore: upgrade React plugin and add node polyfills to Vite config
    - Update `@vitejs/plugin-react` to version `4.0.0`
    - Add `vite-plugin-node-polyfills` as a new plugin in `vite.config.ts`
    
    Signed-off-by: stevecyj <ycsteven.tsao@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,18 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
+import {nodePolyfills} from 'vite-plugin-node-polyfills';
 import * as path from 'path';
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    nodePolyfills({
+      global: true,
+    }),],
   resolve: {
-    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+    alias: [{find: '@', replacement: path.resolve(__dirname, 'src')}],
   },
   define: {
-    global: {},
     'process.env': {},
-    buffer: {},
   },
 });
